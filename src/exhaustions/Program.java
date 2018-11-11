f31e056addbd7862d9278e9757e3015cfe6495db
root <root@DESKTOP-VT5P5N6>
2018-11-11 23:29:55 +0300

    Add main class Operator with operations interfaces

---
+++ src/exhaustions/Program.java
@@ -0,0 +1,58 @@
+package exhaustions;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Scanner;
+import java.time.LocalTime;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Program {    
+    public enum Operator {
+        Add('+', 0, (a, b) -> Math.addExact(a, b)),
+        Subtract('-', 0, (a, b) -> Math.subtractExact(a, b)),
+        Multiply('*', 1, (a, b) -> Math.multiplyExact(a, b));
+        
+        public static final Operator LOWEST = Add;
+        public static final Operator HIGHEST = Multiply;
+        
+        public static final int LOWEST_PRIORITY = 0;
+        public static final int HIGHEST_PRIORITY = 1;
+    
+        private final char symbol;
+        private final int priority;
+        private final CalcFunction function;
+        
+        Operator(char _symbol, int _priority, CalcFunction _function) {
+            symbol = _symbol;
+            priority = _priority;
+            function = _function;
+        }
+        
+        public char getSymbol() {
+            return symbol;
+        }
+        
+        public int getPriority() {
+            return priority;
+        }
+        
+        public CalcFunction getFunction() {
+            return function;
+        }
+        
+        public Operator getNext() {
+            if (this == HIGHEST) return LOWEST;
+            return Operator.values()[ordinal() + 1];
+        }
+    }
+    
+   
+    
+    public interface CalcFunction {
+        public int calc(int a, int b);
+    }
+    
+   
+}
